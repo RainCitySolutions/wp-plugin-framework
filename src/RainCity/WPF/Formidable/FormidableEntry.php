3147d4e340869dc88bc32f4bb8774db3a42ef54b
Blair Cooper <dev@raincitysolutions.com>
2025-06-10 11:29:49 -0700

    Create helpers for int and DateTime values

--- src/RainCity/WPF/Formidable/FormidableEntry.php
+++ src/RainCity/WPF/Formidable/FormidableEntry.php
@@ -9,18 +9,19 @@ abstract class FormidableEntry
     protected \stdClass $entry;
 
     /**
+     * Initialize the Formidable entry.
      *
-     * @param int $entryId
-     * @param string $formKey
-     * @param bool $includeMetaData
+     * @param int $entryId An identifier for a Formidable Forms entry.
+     * @param int|string $formIdKey The id or key for the form the entry belongs to.
+     * @param bool $includeMetaData Whether to include the meta data for the entry.
      */
-    protected function __construct(int $entryId, string $formKey, bool $includeMetaData = true)
+    protected function __construct(int $entryId, int|string $formIdKey, bool $includeMetaData = true)
     {
         if (class_exists(\FrmEntry::class)) {
             /** @var \stdClass|null Get the entry with the meta data */
             $entry = \FrmEntry::getOne($entryId, $includeMetaData);
 
-            if (!is_null($entry) && Formidable::getFormId($formKey) == $entry->formId) {
+            if (!is_null($entry) && $this->isMatchingForm($formIdKey, $entry->formId)) {
                 $this->entry = $entry;
             } else {
                 throw new \InvalidArgumentException('Invalid entryId or not for ROI form');
@@ -30,18 +31,79 @@ abstract class FormidableEntry
         }
     }
 
-    protected function getFieldValue(string $fieldKey): mixed
+    /**
+     * Checks if the specifed form matches the form the entry belongs to.
+     *
+     * @param int|string $formIdKey The id or key for a form
+     * @param int $entryFormId The id form the form associated with the entry
+     *
+     * @return bool True if the formIdKey and the $entryFormId are both the
+     *      same form, otherwise false.
+     */
+    private function isMatchingForm(int|string $formIdKey, int $entryFormId): bool
+    {
+        if (!is_numeric($formIdKey)) {
+            $formIdKey = Formidable::getFormId($formIdKey);
+        }
+
+        return $formIdKey == $entryFormId;
+    }
+
+    /**
+     * Fetch the value for the specified field.
+     *
+     * @param int|string $fieldIdKey The id or key for a form field.
+     *
+     * @return mixed The value of the field, or null if the field is not
+     *      present or the meta data was not requested during object
+     *      construction.
+     */
+    protected function getFieldValue(int|string $fieldIdKey): mixed
     {
         $value = null;
 
         if (isset($this->entry->metas)) {
-            $fieldId = Formidable::getFieldId($fieldKey);
+            if (!is_numeric($fieldIdKey)) {
+                $fieldIdKey = Formidable::getFieldId($fieldIdKey);
+            }
 
-            if (!is_null($fieldId)) {
-                $value = $this->entry->metas[$fieldId] ?? null;
+            if (!is_null($fieldIdKey)) {
+                $value = $this->entry->metas[$fieldIdKey] ?? null;
             }
         }
 
         return $value;
+    }
+
+    /**
+     * Fetch the value for the specified field as an integer.
+     *
+     * @param int|string $fieldIdKey The id or key for a form field.
+     *
+     * @return int|NULL The value of the field or null if the field is not
+     *      present or the meta data was not requested during object
+     *      construction.
+     */
+    protected function getFieldIntValue(int|string $fieldIdKey): ?int
+    {
+        $value = $this->getFieldValue($fieldIdKey);
+
+        return !is_null($value) && is_numeric($value) ? intval($value) : null;
+    }
+
+    /**
+     * Fetch the value for the specified field as a \DateTime instance
+     *
+     * @param int|string $fieldIdKey The id or key for a form field.
+     *
+     * @return \DateTime|NULL The value of the field or null if the field is not
+     *      present or the meta data was not requested during object
+     *      construction.
+     */
+    protected function getFieldDateValue(int|string $fieldIdKey): ?\DateTime
+    {
+        $dateStr = $this->getFieldValue($fieldIdKey);
+
+        return !is_null($dateStr) ? new \DateTime($dateStr) : null;
     }
 }
